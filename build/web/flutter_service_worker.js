847d7e15b10442ff19dd873325dafc5ec779af9b
Linaks <inangokay@hotmail.com>
2020-07-12 17:49:04 +0200

    Fix

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -32,7 +32,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "736be9b760334a2e0d45cea2b63a9959",
 "/": "736be9b760334a2e0d45cea2b63a9959",
-"main.dart.js": "7aab57a0340f2f895024ef89779d1611",
+"main.dart.js": "075340a4e383e425da23a73fb9258cbf",
 "manifest.json": "d38deda368e948b9ffcbfaa512bc07f5"
 };
 
