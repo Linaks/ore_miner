77e85a75221013a48aff436b5d451e41eb2a47bc
Linaks <inangokay@hotmail.com>
2020-07-12 20:54:27 +0200

    Name Fix

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -32,7 +32,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "736be9b760334a2e0d45cea2b63a9959",
 "/": "736be9b760334a2e0d45cea2b63a9959",
-"main.dart.js": "ab9177b5b4b754a29225e232f6d19abe",
+"main.dart.js": "63df89e9be715e545e5f7876f508182d",
 "manifest.json": "d38deda368e948b9ffcbfaa512bc07f5"
 };
 
